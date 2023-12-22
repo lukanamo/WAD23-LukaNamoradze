7457a5ee486ce2fbc90cf5fba9bab1a9f1331f2a
Luka Namoradze <namoo7@icloud.com>
2023-12-22 14:38:44 +0200

    Task 1

---
+++ src/components/ACourse.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="course">
+    <h4>{{ course.coursename }}</h4>
+    <p>Code: {{ course.coursecode }}</p>
+    <p>ECTS: {{ course.courseects }}</p>
+    <p>Max No. of Students: {{ course.studentsnumbers }}</p>
+    <p>No. of groups: {{ course.groupsnumbers }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Course",
+  props: {
+    course: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.course {
+  background-color: #b58df9;
+  margin: 0.5rem;
+  padding: 1rem;
+  border-radius: 4px;
+}
+
+h4 {
+  color: #010101;
+}
+</style>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,15 +1,19 @@
 <template>
-<h3>Courses</h3>
-
-
+<div>
+    <h3>Courses</h3>
+    <div v-for="course in courses" :key="course.id">
+      <Course :course="course" />
+    </div>
+</div>
 </template>
 
 <script>
+import Course from '@/components/ACourse.vue';
 
 export default {
   name: "Courses",
     components: {
-
+      Course
     },
   data() {
     return {
